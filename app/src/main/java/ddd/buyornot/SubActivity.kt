94ea006f73e3e06f591685ab5f2ef58183f1a673
심은석 <eushim@gmarket.com>
2023-06-15 22:36:27 +0900

    sharesheet에 앱 노출 및 데이터 받아오기 예제

---
+++ app/src/main/java/ddd/buyornot/SubActivity.kt
@@ -0,0 +1,33 @@
+package ddd.buyornot
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import ddd.buyornot.ui.theme.BuyOrNotTheme
+
+class SubActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            BuyOrNotTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+
+                    when (intent?.action) {
+                        Intent.ACTION_SEND -> {
+                            if (intent?.type?.contains("text") == true) {
+                                // Greeting(intent.getStringExtra(Intent.EXTRA_TEXT) ?: "null")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
