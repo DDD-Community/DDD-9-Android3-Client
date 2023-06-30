4f3d5bbe5871f33faa9a1556683d3c090c886d28
심은석 <eushim@gmarket.com>
2023-06-30 22:56:18 +0900

    미사용 import 제거

--- app/src/main/java/ddd/buyornot/LoginActivity.kt
+++ app/src/main/java/ddd/buyornot/LoginActivity.kt
@@ -2,7 +2,6 @@ package ddd.buyornot
 
 import android.os.Bundle
 import android.os.PersistableBundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,7 +9,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.ui.Modifier
 import com.ddd.component.BDSImage
 import com.ddd.component.theme.BuyOrNotTheme
-import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
 import ddd.buyornot.data.service.LoginService
 import ddd.buyornot.login.KakaoLogin
