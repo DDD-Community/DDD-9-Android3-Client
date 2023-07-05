750ddad54497fbeb6d244795e0d5eafe7174d39d
심은석 <eushim@gmarket.com>
2023-07-05 23:17:13 +0900

    Material3 ModalBottomSheet 사용

--- component-demo/src/main/java/com/ddd/component/demo/DemoHomeActivity.kt
+++ component-demo/src/main/java/com/ddd/component/demo/DemoHomeActivity.kt
@@ -13,15 +13,18 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -31,9 +34,8 @@ import com.ddd.component.BDSBottomSheetLayout
 import com.ddd.component.BDSText
 import com.ddd.component.theme.BDSFontFamily
 import com.ddd.component.theme.BuyOrNotTheme
-import kotlinx.coroutines.launch
 
-@ExperimentalMaterialApi
+@ExperimentalMaterial3Api
 class DemoHomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,106 +55,106 @@ class DemoHomeActivity : ComponentActivity() {
 }
 
 @Composable
-@ExperimentalMaterialApi
+@ExperimentalMaterial3Api
 fun DemoHomeScreen(
     navController: NavHostController
 ) {
-    DemoBottomSheet(navController)
+    val scrollState = rememberScrollState()
+    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
+    var skipPartiallyExpanded by remember { mutableStateOf(false) }
+    val bottomSheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = skipPartiallyExpanded
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+            .padding(vertical = 24.dp, horizontal = 16.dp)
+    ) {
+        Button(
+            onClick = {
+                navController.navigate(DemoNavigationRoute.Typography.route)
+            }) {
+            Text(
+                text = "Typography Test",
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+        Button(
+            modifier = Modifier.padding(top = 8.dp),
+            onClick = {
+                navController.navigate(DemoNavigationRoute.Image.route)
+            }) {
+            Text(
+                text = "Image Test",
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+        Button(
+            modifier = Modifier.padding(top = 8.dp),
+            onClick = {
+                openBottomSheet = !openBottomSheet
+            }) {
+            Text(
+                text = "BottomSheet Test",
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+    }
+
+    if (openBottomSheet) {
+        DemoBottomSheet(
+            onDismissRequest = { openBottomSheet = false}
+        )
+    }
 }
 
 @Composable
-@ExperimentalMaterialApi
+@ExperimentalMaterial3Api
 fun DemoBottomSheet(
-    navController: NavHostController
+    onDismissRequest: () -> Unit
 ) {
-    val scrollState = rememberScrollState()
-    val coroutineScope = rememberCoroutineScope()
-    val sheetState = rememberModalBottomSheetState(
-        initialValue = ModalBottomSheetValue.Hidden
-    )
-
     BDSBottomSheetLayout(
-        coroutineScope = coroutineScope,
-        sheetState = sheetState,
-        sheetContent = {
-            Box(modifier = Modifier.fillMaxWidth().height(224.dp)) {
-                BDSText(
-                    text = "상품을 삭제할까요?",
-                    style = MaterialTheme.typography.titleLarge,
-                    fontFamily = BDSFontFamily.English,
-                    modifier = Modifier
-                        .offset(y = 52.dp)
-                        .align(Alignment.TopCenter)
-                )
-                BDSText(
-                    text = "선택하신 상품을 정말 삭제하시겠어요?",
-                    style = MaterialTheme.typography.titleMedium,
-                    modifier = Modifier
-                        .offset(y = 86.dp)
-                        .align(Alignment.TopCenter)
-                )
-                Row(
-                    modifier = Modifier
-                        .offset(y = 144.dp)
-                        .align(Alignment.TopCenter)
-                ) {
-                    Button(onClick = { /*TODO*/ }) {
-                        BDSText(
-                            text = "취소"
-                        )
-                    }
-                    Button(onClick = { /*TODO*/ }) {
-                        BDSText(
-                            text = "삭제"
-                        )
-                    }
-                }
-            }
-        },
+        onDismissRequest = onDismissRequest,
+        dragHandle = null
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(scrollState)
-                .padding(vertical = 24.dp, horizontal = 16.dp)
-        ) {
-            Button(
-                onClick = {
-                    navController.navigate(DemoNavigationRoute.Typography.route)
-                }) {
-                Text(
-                    text = "Typography Test",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
-            }
-            Button(
-                modifier = Modifier.padding(top = 8.dp),
-                onClick = {
-                    navController.navigate(DemoNavigationRoute.Image.route)
-                }) {
-                Text(
-                    text = "Image Test",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
-            }
-            Button(
-                modifier = Modifier.padding(top = 8.dp),
-                onClick = {
-                    coroutineScope.launch {
-                        if (sheetState.isVisible) {
-                            sheetState.hide()
-                        } else {
-                            sheetState.animateTo(ModalBottomSheetValue.Expanded)
-                        }
-                    }
-                }) {
-                Text(
-                    text = "BottomSheet Test",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(224.dp)) {
+            BDSText(
+                text = "상품을 삭제할까요?",
+                style = MaterialTheme.typography.titleLarge,
+                fontFamily = BDSFontFamily.English,
+                modifier = Modifier
+                    .offset(y = 52.dp)
+                    .align(Alignment.TopCenter)
+            )
+            BDSText(
+                text = "선택하신 상품을 정말 삭제하시겠어요?",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier
+                    .offset(y = 86.dp)
+                    .align(Alignment.TopCenter)
+            )
+            Row(
+                modifier = Modifier
+                    .offset(y = 144.dp)
+                    .align(Alignment.TopCenter)
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    BDSText(
+                        text = "취소"
+                    )
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    BDSText(
+                        text = "삭제"
+                    )
+                }
             }
         }
     }
--- component/src/main/kotlin/com/ddd/component/BottomSheet.kt
+++ component/src/main/kotlin/com/ddd/component/BottomSheet.kt
@@ -1,43 +1,64 @@
 package com.ddd.component
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetLayout
-import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 /**
  * BDSBottomSheet
  *
  * 화면 하단에 Composable Content를 노출하는 Sheet
- * sheetContent : BottomSheet 내부에 넣을 Composable Content
- * content : BottomSheet를 감싸는 Screen Composable Content
+ * modifier: 그냥 Modifier
+ * onDismissRequest: 바텀 시트가 사라짐(hidden) 애니메이션이 끝난 후에 실행할 동작
+ * sheetState: bottom sheet 상태, 보여지고 있는지 숨김 상태인지 전체 확장 상태인지 등
+ * shape: bottom sheet 모양
+ * containerColor: bottom sheet의 배경색
+ * contentColor: bottom sheet content 기본 색상. containerColor에 대해 일치하는 content color 색상
+ * 또는 containerColor가 테마의 색상이 아닌 경우 현재 LocalContentColor로 기본 설정
+ * tonalElevation: bottom sheet 색조 elevation
+ * scrimColor: bottom sheet가 열려있을 때 content를 가리는 scrim 색
+ * dragHandle: bottom sheet 스와이프용 핸들
+ * content: bottom sheet 내부에 그릴 Composable
  */
 
 @Composable
-@ExperimentalMaterialApi
+@ExperimentalMaterial3Api
 fun BDSBottomSheetLayout(
-    coroutineScope: CoroutineScope,
-    sheetState: ModalBottomSheetState,
-    sheetContent: @Composable ColumnScope.() -> Unit,
-    content: @Composable () -> Unit
+    modifier: Modifier = Modifier,
+    onDismissRequest: () -> Unit = {},
+    sheetState: SheetState = rememberModalBottomSheetState(),
+    shape: Shape = RoundedCornerShape(
+        topStart = 16.dp,
+        topEnd = 16.dp
+    ),
+    containerColor: Color = BottomSheetDefaults.ContainerColor,
+    contentColor: Color = contentColorFor(containerColor),
+    tonalElevation: Dp = BottomSheetDefaults.Elevation,
+    scrimColor: Color = BottomSheetDefaults.ScrimColor,
+    dragHandle: @Composable (() -> Unit)? = { BottomSheetDefaults.DragHandle() },
+    content: @Composable ColumnScope.() -> Unit
 ) {
-    val roundedCornerShape = 16.dp
-
-    BackHandler(sheetState.isVisible) {
-        coroutineScope.launch { sheetState.hide() }
-    }
-
-    ModalBottomSheetLayout(
-        sheetContent =  sheetContent,
+    ModalBottomSheet(
+        modifier = modifier,
         sheetState = sheetState,
-        sheetShape = RoundedCornerShape(topStart = roundedCornerShape, topEnd = roundedCornerShape),
+        onDismissRequest = onDismissRequest,
+        shape = shape,
+        containerColor = containerColor,
+        contentColor = contentColor,
+        tonalElevation = tonalElevation,
+        scrimColor = scrimColor,
+        dragHandle = dragHandle,
         content = content
     )
 }
-
