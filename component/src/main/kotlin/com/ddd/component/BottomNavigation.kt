a84f2280b82774e274fcbe27b213e528eb3dde10
zion830 <zion830@naver.com>
2023-07-06 20:57:14 +0900

    bottomNavigation 아이콘, 색상 수정

--- component/src/main/kotlin/com/ddd/component/BottomNavigation.kt
+++ component/src/main/kotlin/com/ddd/component/BottomNavigation.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,9 +20,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ddd.component.BottomNavigationItem.Companion.bottomNavigationItems
-import com.ddd.component.theme.Gray500
+import com.ddd.component.theme.Primary400
+import com.ddd.component.theme.SlateGray500
+import com.ddd.component.theme.SlateGray600
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,19 +42,34 @@ fun BDSBottomNavigation(
                 modifier = Modifier.fillMaxWidth(),
                 tonalElevation = 8.dp,
                 containerColor = Color.White,
-                contentColor = Gray500,
+                contentColor = SlateGray500,
             ) {
                 bottomNavigationItems.forEach { item ->
+                    val isSelected = selectedItem == item
+
                     NavigationBarItem(
-                        selected = selectedItem == item,
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = Primary400,
+                            selectedTextColor = Primary400,
+                            unselectedIconColor = SlateGray500,
+                            unselectedTextColor = SlateGray600,
+                            indicatorColor = Color.White,
+                        ),
+                        selected = isSelected,
                         icon = {
                             Icon(
                                 painter = painterResource(id = item.icon),
-                                contentDescription = item.title
+                                contentDescription = item.title,
                             )
                         },
                         label = item.title?.let { text ->
-                            { Text(text = text) }
+                            {
+                                Text(
+                                    text = text,
+                                    fontSize = 11.sp,
+                                    fontWeight = FontWeight.Bold,
+                                )
+                            }
                         },
                         onClick = {
                             selectedItem = item
@@ -73,9 +93,9 @@ sealed class BottomNavigationItem(
     val route: String
 ) {
 
-    object Home : BottomNavigationItem("홈", android.R.drawable.ic_menu_manage, "home")
-    object Add : BottomNavigationItem(null, android.R.drawable.ic_menu_add, "add")
-    object Archive : BottomNavigationItem("아카이브", android.R.drawable.ic_menu_manage, "archive")
+    object Home : BottomNavigationItem("홈", R.drawable.ic_house, "home")
+    object Add : BottomNavigationItem(null, R.drawable.ic_add_archive, "add")
+    object Archive : BottomNavigationItem("아카이브", R.drawable.ic_heart_mono_fill, "archive")
 
     companion object {
         val bottomNavigationItems = listOf(
