a72d9cac032c386aca0ed61019dc50134b814498
eunseok Shim <ses8323@gmail.com>
2023-07-20 15:44:08 +0900

    BDSBottomSheet 주석 추가

--- component/src/main/kotlin/com/ddd/component/BDSBottomSheet.kt
+++ component/src/main/kotlin/com/ddd/component/BDSBottomSheet.kt
@@ -28,61 +28,18 @@ import com.holix.android.bottomsheetdialog.compose.BottomSheetDialog
 import com.holix.android.bottomsheetdialog.compose.BottomSheetDialogProperties
 
 /**
+ * [BottomSheetDialog github](https://github.com/holixfactory/bottomsheetdialog-compose)
  * BDSBottomSheet
  * 화면 하단에 Composable Content를 노출하는 Sheet
  * 높이가 가변적으로 컨텐츠 길이에 따라 높이가 변동 가능
- * top margin 최소 72dp
+ * 최대 높이는 전체 화면의 90%
  * 사용자가 높이를 직접 줄일 수 없음
- * modifier: 그냥 Modifier
- * onDismissRequest: 바텀 시트가 사라짐(hidden) 애니메이션이 끝난 후에 실행할 동작
- * sheetState: bottom sheet 상태, 보여지고 있는지 숨김 상태인지 전체 확장 상태인지 등
- * shape: bottom sheet 모양
- * containerColor: bottom sheet의 배경색
- * contentColor: bottom sheet content 기본 색상. containerColor에 대해 일치하는 content color 색상
- * 또는 containerColor가 테마의 색상이 아닌 경우 현재 LocalContentColor로 기본 설정
- * tonalElevation: bottom sheet 색조 elevation
- * scrimColor: bottom sheet가 열려있을 때 content를 가리는 scrim 색
- * dragHandle: bottom sheet 스와이프용 핸들
- * content: bottom sheet 내부에 그릴 Composable
+ * onDismissRequest: 바텀 시트가 사라짐(hidden, collapse) 애니메이션이 끝난 후에 실행할 동작
+ * properties: 바텀 시트가 가질 수 있는 속성을 변경할 수 있음 (expand 상태, draggable 등)
+ * headerContent: 바텀 시트 상단에 추가될 컴포저블 함수로 주로 BDSBottomSheetHeader를 사용
+ * bodyContent: 바텀 시트 중앙에 추가될 컴포저블 함수
+ * bottomContent: 바텀 시트 하단에 추가될 컴포저블 함수로 주로 BDSBottomSheet*Button 시리즈를 사용
  */
-
-/*@Composable
-@ExperimentalMaterial3Api
-fun BDSBottomSheetLayout(
-    modifier: Modifier = Modifier,
-    onDismissRequest: () -> Unit = {},
-    sheetState: SheetState = rememberModalBottomSheetState(),
-    shape: Shape = RoundedCornerShape(
-        topStart = 16.dp,
-        topEnd = 16.dp
-    ),
-    containerColor: Color = BottomSheetDefaults.ContainerColor,
-    contentColor: Color = contentColorFor(containerColor),
-    tonalElevation: Dp = BottomSheetDefaults.Elevation,
-    scrimColor: Color = BottomSheetDefaults.ScrimColor,
-    dragHandle: @Composable (() -> Unit)? = null,
-    headerContent: @Composable (() -> Unit)? = null,
-    bodyContent: @Composable (() -> Unit)? = null,
-    buttonContent: @Composable (() -> Unit)? = null
-) {
-    ModalBottomSheet(
-        modifier = Modifier,
-        onDismissRequest = onDismissRequest,
-        sheetState = sheetState,
-        shape = shape,
-        containerColor = containerColor,
-        contentColor = contentColor,
-        tonalElevation = tonalElevation,
-        scrimColor = scrimColor,
-        dragHandle = dragHandle,
-        content = {
-            headerContent?.invoke()
-            bodyContent?.invoke()
-            buttonContent?.invoke()
-        }
-    )
-}
-}*/
 
 @Composable
 fun BDSBottomSheet(
@@ -139,7 +96,6 @@ fun BDSBottomSheetPostList(
         Box(
             modifier = Modifier
                 .wrapContentHeight()
-                // .padding(top = 72.dp)
                 .fillMaxHeight(0.9f)
                 .background(
                     color = Color.White,
