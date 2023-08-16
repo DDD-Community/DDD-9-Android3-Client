7ea5df950f5cfe2f9bbc122c7a2091d8a01e0d49
simeunseok <ses8323@gmail.com>
2023-08-16 15:53:25 +0900

    Post 모듈 추가

--- data/src/main/java/ddd/buyornot/data/di/PostModule.kt
+++ data/src/main/java/ddd/buyornot/data/di/PostModule.kt
@@ -1,4 +1,31 @@
 package ddd.buyornot.data.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ddd.buyornot.data.repository.post.PostRepository
+import ddd.buyornot.data.repository.post.PostRepositoryImpl
+import ddd.buyornot.data.service.PostService
+import ddd.buyornot.data.source.post.PostRemoteDataSource
+import ddd.buyornot.data.source.post.PostRemoteDataSourceImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object PostModule {
+
+    @Provides
+    @Singleton
+    fun providePostRemoteDataSource(
+        postService: PostService
+    ): PostRemoteDataSource = PostRemoteDataSourceImpl(postService)
+
+    @Provides
+    @Singleton
+    fun providePostRepository(
+        postRemoteDataSource: PostRemoteDataSource
+    ): PostRepository = PostRepositoryImpl(postRemoteDataSource)
+
+
 }
