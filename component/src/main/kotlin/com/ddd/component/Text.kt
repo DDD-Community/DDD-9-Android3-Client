421d9c1482c0ee511752b7781aabf3a798b55702
simeunseok <ses8323@gmail.com>
2023-09-19 16:50:05 +0900

    dpToSp로 BDSText fontsize 고정

--- component/src/main/kotlin/com/ddd/component/Text.kt
+++ component/src/main/kotlin/com/ddd/component/Text.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
@@ -15,9 +16,51 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import com.ddd.component.theme.BDSFontFamily
+
+@Composable
+fun BDSText(
+    text: String?,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: Dp = Dp.Unspecified,
+    fontStyle: FontStyle? = FontStyle.Normal,
+    fontWeight: FontWeight? = FontWeight.Normal,
+    fontFamily: BDSFontFamily? = BDSFontFamily.Korean,
+    letterSpacing: TextUnit = (-0.3).sp,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = TextAlign.Start,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    inlineContent: Map<String, InlineTextContent> = mapOf(),
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = LocalTextStyle.current
+) {
+    BDSText(
+        text = buildAnnotatedString { append(text ?: "") },
+        modifier = modifier,
+        color = color,
+        fontSize = dpToSp(dp = fontSize),
+        fontStyle = fontStyle ?: FontStyle.Normal,
+        fontWeight = fontWeight ?: FontWeight.Normal,
+        fontFamily = fontFamily,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        inlineContent = inlineContent,
+        onTextLayout = onTextLayout,
+        style = style
+    )
+}
 
 @Composable
 fun BDSText(
@@ -100,3 +143,6 @@ fun BDSText(
         style = style
     )
 }
+
+@Composable
+private fun dpToSp(dp: Dp) = with(LocalDensity.current) { dp.toSp() }
