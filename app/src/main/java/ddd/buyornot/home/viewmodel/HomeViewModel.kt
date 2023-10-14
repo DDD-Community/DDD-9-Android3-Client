32500704ace35821650574d48728dba4f3060019
simeunseok <ses8323@gmail.com>
2023-10-14 16:48:03 +0900

    메인 화면 좋아요 클릭 시 아카이브함에 아이템 담기

--- app/src/main/java/ddd/buyornot/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/ddd/buyornot/home/viewmodel/HomeViewModel.kt
@@ -7,8 +7,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ddd.component.data.SnackbarUi
 import dagger.hilt.android.lifecycle.HiltViewModel
+import ddd.buyornot.data.model.archive.DeleteArchiveReq
 import ddd.buyornot.data.model.post.PostResult
 import ddd.buyornot.data.prefs.SharedPreferenceWrapper
+import ddd.buyornot.data.repository.archive.ArchiveRepository
 import ddd.buyornot.data.repository.poll.PollRepository
 import ddd.buyornot.data.repository.post.PostRepository
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -20,7 +22,8 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     private val sharedPreferenceWrapper: SharedPreferenceWrapper,
     private val postRepository: PostRepository,
-    private val pollRepository: PollRepository
+    private val pollRepository: PollRepository,
+    private val archiveRepository: ArchiveRepository
 ) : ViewModel() {
 
     private var page = 0
@@ -29,7 +32,7 @@ class HomeViewModel @Inject constructor(
     val profile = sharedPreferenceWrapper.profile
 
     private val _uiEvent = MutableSharedFlow<SnackbarUi>()
-    val uiEvent : SharedFlow<SnackbarUi>
+    val uiEvent: SharedFlow<SnackbarUi>
         get() = _uiEvent
 
     private val _isRefresh = mutableStateOf(false)
@@ -71,14 +74,25 @@ class HomeViewModel @Inject constructor(
         }
     }
 
-    suspend fun postArchiveItem() {
+    private suspend fun postArchiveItem(itemId: Int) {
         viewModelScope.launch {
-            // TODO: 아카이브함 상품 담기 추가 
-            /*if (sharedItemUrl.isNotEmpty()) {
-                val result = archiveRepository.postArchiveItem(sharedItemUrl)
-                archiveRepository.postArchiveItem(sharedItemUrl)
-            }*/
+            archiveRepository.postArchiveItem(itemId)
             _uiEvent.emit(SnackbarUi.POST_ARCHIVE)
+        }
+    }
+
+    private suspend fun patchArchiveItemDelete(itemId: Int) {
+        viewModelScope.launch {
+            archiveRepository.patchArchiveItemDelete(DeleteArchiveReq(listOf(itemId)))
+            _uiEvent.emit(SnackbarUi.DELETE_ITEM)
+        }
+    }
+
+    suspend fun clickLikeButton(isLike: Boolean, itemId: Int) {
+        if (isLike) {
+            patchArchiveItemDelete(itemId)
+        } else {
+            postArchiveItem(itemId)
         }
     }
 }
